44078f288de74b633a4decbbba0f8635ece0607b
OlehMaliuta <104268973+OlehMaliuta@users.noreply.github.com>
2023-05-07 15:39:08 +0300

    Main window and settings improvements * added the main activity; * changed some variables; * changed versions of plugins.

---
+++ app/src/main/java/com/nkeva/eduplatform/MainActivity.java
@@ -0,0 +1,14 @@
+package com.nkeva.eduplatform;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
